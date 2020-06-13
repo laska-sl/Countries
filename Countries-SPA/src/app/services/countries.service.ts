7a6183a622ddaeac62c95ddb5e404158da1038dc
laska_sl <lasukovv902@gmail.com>
2020-06-13 11:16:02 +0300

    Chagned subject subscription

--- Countries-SPA/src/app/services/countries.service.ts
+++ Countries-SPA/src/app/services/countries.service.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 })
 export class CountriesService {
   baseUrl = environment.apiUrl + 'countries/';
-  countryCreated = new Subject<Country>();
+  countryCreated = new Subject();
 
   constructor(private http: HttpClient) { }
 
